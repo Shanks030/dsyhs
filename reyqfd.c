2ad1428b411f3fa3acfc87126f4029c19ead17af
Shanks030 <122090882+Shanks030@users.noreply.github.com>
2023-04-02 15:50:41 +0000

    Create reyqfd.c

---
+++ reyqfd.c
@@ -0,0 +1,39 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int choice1;
+    int D-choice;
+    printf("welcome to the store ,we hope that you will have such a great experience! ");
+    printf("1-drinks\n2-sand");
+    printf("please select a number");
+    scanf("%d",&choice1);
+    if (choice1==1){
+        printf("1-1 oz. vodka\n2-coke");
+        scanf("%d",&D-choice);
+        if (D-choice==1){
+            printf("great choice please enter your creadit card's N°");
+            scanf("%ld",&creditcard);
+            if (creditcard>=1000000000000000){
+                printf("sucsses !");
+            }
+            else{
+                printf("sorry!");
+            }
+        }
+        if else(D-choice==2){
+            printf("great choice please enter your creadit card's N°");
+            scanf("%ld",&creditcard);
+            if (creditcard>=1000000000000000){
+                printf("sucsses !");
+            }
+            else{
+                printf("sorry!");
+            
+        }
+            
+        }
+    }
+    if else()
+    return 0;
+}
