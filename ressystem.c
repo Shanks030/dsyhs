7f2babe8905dcafcf0cd0698d93c52939a15734b
Shanks030 <122090882+Shanks030@users.noreply.github.com>
2023-04-13 23:45:31 +0000

    Update ressystem.c

--- ressystem.c
+++ ressystem.c
@@ -4,25 +4,28 @@ int main() {
     int choice1;
     int D_choice;
 	int S_choice;
+	long int creditcard;
     printf("welcome to the store ,we hope that you will have such a great experience! ");
-    printf("1-drinks\n2-sand");
-    printf("please select a number");
+    printf("\n1-drinks\n2-sand");
+    printf("\nplease select a number :");
     scanf("%d",&choice1);
     if (choice1==1){
         printf("1-1 oz. vodka\n2-coke");
         scanf("%d",&D_choice);
-        if (D-choice==1){
-            printf("great choice please enter your creadit card's N°");
+        if (D_choice==1){
+            printf("great choice please enter your creadit card's N°:");
             scanf("%ld",&creditcard);
             if (creditcard>=1000000000000000){
-                printf("sucsses !");
+                printf("succses !");
+                return 0 ;
             }
             else{
                 printf("sorry!");
+                return 0 ;
             }
         }
-        if else(D_choice==2){
-            printf("great choice please enter your creadit card's N°");
+        else if(D_choice==2){
+            printf("great choice please enter your creadit card's N°:");
             scanf("%ld",&creditcard);
             if (creditcard>=1000000000000000){
                 printf("sucsses !");
@@ -34,12 +37,12 @@ int main() {
 
         }
     }
-    if else(choice1==2){
+    else if(choice1==2){
         printf("Please choose one \n 1-Turkey and Cranberry Sandwich \n 2-Grilled Cheese and Bacon Sandwich.");
 		printf("\n please choose a number 1 or 2:");
 		scanf("%d",&S_choice);
 		 if (S_choice==1){
-            printf("great choice please enter your creadit card's N°");
+            printf("great choice please enter your creadit card's N°:");
             scanf("%ld",&creditcard);
             if (creditcard>=1000000000000000){
                 printf("sucsses !");
@@ -48,8 +51,8 @@ int main() {
                 printf("sorry!");
             }
         }
-        if else(S_choice==2){
-            printf("great choice please enter your creadit card's N°");
+        else if(S_choice==2){
+            printf("great choice please enter your creadit card's N°:");
             scanf("%ld",&creditcard);
             if (creditcard>=1000000000000000){
                 printf("sucsses !");
