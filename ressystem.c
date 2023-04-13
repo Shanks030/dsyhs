5703f11fe09bddeaa8551b8fee1f35ad79da895c
Shanks030 <122090882+Shanks030@users.noreply.github.com>
2023-04-13 23:19:24 +0000

    Update ressystem.c

--- ressystem.c
+++ ressystem.c
@@ -1,16 +1,16 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
     int choice1;
-    int D-choice;
+    int D_choice;
+	int S_choice;
     printf("welcome to the store ,we hope that you will have such a great experience! ");
     printf("1-drinks\n2-sand");
     printf("please select a number");
     scanf("%d",&choice1);
     if (choice1==1){
         printf("1-1 oz. vodka\n2-coke");
-        scanf("%d",&D-choice);
+        scanf("%d",&D_choice);
         if (D-choice==1){
             printf("great choice please enter your creadit card's N°");
             scanf("%ld",&creditcard);
@@ -21,7 +21,7 @@ int main() {
                 printf("sorry!");
             }
         }
-        if else(D-choice==2){
+        if else(D_choice==2){
             printf("great choice please enter your creadit card's N°");
             scanf("%ld",&creditcard);
             if (creditcard>=1000000000000000){
@@ -29,11 +29,38 @@ int main() {
             }
             else{
                 printf("sorry!");
-            
+
         }
-            
+
         }
     }
-    if else()
+    if else(choice1==2){
+        printf("Please choose one \n 1-Turkey and Cranberry Sandwich \n 2-Grilled Cheese and Bacon Sandwich.");
+		printf("\n please choose a number 1 or 2:");
+		scanf("%d",&S_choice);
+		 if (S_choice==1){
+            printf("great choice please enter your creadit card's N°");
+            scanf("%ld",&creditcard);
+            if (creditcard>=1000000000000000){
+                printf("sucsses !");
+            }
+            else{
+                printf("sorry!");
+            }
+        }
+        if else(S_choice==2){
+            printf("great choice please enter your creadit card's N°");
+            scanf("%ld",&creditcard);
+            if (creditcard>=1000000000000000){
+                printf("sucsses !");
+            }
+            else{
+                printf("sorry!");
+
+        }
+
+        }
+
+    }
     return 0;
 }
